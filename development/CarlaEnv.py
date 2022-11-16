8e617cceafecb2d23a84f5f88302fe7df74e6781
xfranv8 <fv8vazquez@gmail.com>
2022-11-16 21:10:58 +0100

    Reset function implemented

--- development/CarlaEnv.py
+++ development/CarlaEnv.py
@@ -1,4 +1,4 @@
-import glob, os, sys, carla, random, time
+import glob, os, sys, carla, random, time, numpy as np
 
 
 class CarlaEnv:
@@ -9,9 +9,60 @@ class CarlaEnv:
         self.client.load_world(town)
         self.client.set_timeout(20.0)
         self.world: carla.World = self.client.get_world()
-        self.blueprint_library = self.world.get_blueprint_library()
         self.world.set_weather(weather)
-        self.model3: carla.Vehicle = self.blueprint_library.filter("model3")[0]
+
+        self._actors = []
+        self._data_dict = {}
+        self._collision_hist = []
+        self._vehicle: carla.Vehicle
+
+    def step(self, action):
+        self._vehicle.apply_control(carla.VehicleControl(throttle=1, steer=0))
 
     def reset(self):
-        pass
+        if len(self._collision_hist) > 0:
+            for actor in self._actors:
+                actor.destroy()
+                self._collision_hist = []
+                return True
+        return False
+
+    def spawn_vehicle(self, model: carla.BlueprintLibrary, camera=None, collision_detector=None):
+        # spawn_point = carla.Transform(carla.Location(x=41.389999, y=275.029999, z=0.500000),
+        # carla.Rotation(pitch=0.000000, yaw=89.999954, roll=0.000000))
+
+        spawn_point = random.choice(self.world.get_map().get_spawn_points())
+
+        self._vehicle = self.world.spawn_actor(model, spawn_point)
+        self._actors.append(self._vehicle)
+
+        transform = carla.Transform(carla.Location(x=2.5, z=0.7))
+        if camera is not None:
+            camera.set_attribute("image_size_x", "640")
+            camera.set_attribute("image_size_y", "480")
+            camera.set_attribute("fov", "110")
+
+            sensor = self.world.spawn_actor(camera, transform, attach_to=self._vehicle)
+            self._actors.append(sensor)
+            camera_data = {"image": np.zeros((480, 640, 4))}
+            sensor.listen(lambda data: self._process_image(data))
+        if collision_detector is not None:
+            colsensor = self.world.get_blueprint_library().find('sensor.other.collision')
+            self.colsensor = self.world.spawn_actor(collision_detector, transform, attach_to=self._vehicle)
+            self._actors.append(self.colsensor)
+            self.colsensor.listen(lambda event: self._collision_data(event))
+
+    def get_blueprint_library(self):
+        return self.world.get_blueprint_library()
+
+    def _process_image(self, data):
+        """Convert a CARLA raw image to a BGRA numpy array."""
+        if not isinstance(data, carla.Image):
+            raise ValueError("Argument must be a carla.Image")
+        image = np.array(data.raw_data)
+        image2 = image.reshape((480, 640, 4))
+        # image3 = image2[:, :, :3]
+        self._data_dict['image'] = image2
+
+    def _collision_data(self, event):
+        self._collision_hist.append(event)
--- development/main.py
+++ development/main.py
@@ -9,6 +9,16 @@ def main():
         sun_altitude_angle=0.0)
     town = "Town02"
     environment = CarlaEnv(town, weather)
+    blueprint_library: carla.BlueprintLibrary = environment.get_blueprint_library()
+
+    model = blueprint_library.filter("model3")[0]
+    cam = blueprint_library.find("sensor.camera.rgb")
+    colsensor = blueprint_library.find('sensor.other.collision')
+    environment.spawn_vehicle(model, cam, colsensor)
+    environment.step(None)
+    aux = False
+    while not aux:
+        aux = environment.reset()
 
 
 if __name__ == '__main__':
